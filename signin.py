a50a5c1d832340b6449c7750686e13ef39d9e9e7
shreshta2000 <69512364+shreshta2000@users.noreply.github.com>
2021-02-24 12:02:54 +0530

    Update signin.py

--- signin.py
+++ signin.py
@@ -5,22 +5,21 @@ password1 = input("entre your password ")
 my_dic = {"First_Name":first_name,"Last_Name":last_name,"Password":password1}
 my_dic=str(my_dic)
 if os.path.isfile(first_name + last_name+".txt"):
-    print("this username already exist")
-    for_login = input("you want to login or not respectively y/n ")
+    print("This username already exist")
+    for_login = input("you want to login or not y/n ").lower()
     if for_login == "y":
         username = input("entre your fullname ")
         password = input("entre your password ")
-        if username == first_name + last_name:
-            if password1 == password:
-                print("login successful")
-            else:
-                print("wrong password")
+        if username == first_name + last_name and  password1 == password:
+            print("Login Succesful!")
         else:
-            print("wrong username")
+            print("Incorrect username or Password")
     else:
-        print("not login")
+        print("Thank You")
 else:
     my_file = open(first_name + last_name+".txt","a")
     file1 = my_file.write(my_dic)
+    print("SignIn Succesful!")
+
      
     
